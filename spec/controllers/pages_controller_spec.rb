a0bef6a3097e1f05f85461b25e547900872bcb35
Steve Nelson <stv.nlsn@gmail.com>
2011-01-13 12:42:09 -0500

    Added Help page and modified RSpec tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,17 +3,23 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-    
+
     it "should have the right title" do
-          get 'home'
-          response.should have_selector("title",
-                            :content => "Ruby on Rails Tutorial Sample App | Home")
-        end
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -22,28 +28,23 @@ describe PagesController do
       response.should be_success
     end
 
-      it "should have the right title" do
-        get 'contact'
-        response.should have_selector("title",
-                          :content =>
-                            "Ruby on Rails Tutorial Sample App | Contact")
-      end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
+    end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
 
-      it "should have the right title" do
-        get 'about'
-        response.should have_selector("title",
-                          :content =>
-                            "Ruby on Rails Tutorial Sample App | About")
-      end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
+    end
   end
-  
-  
-
 end
